e6f9db74aa58a55979ecfda72bdba533fa58784c
AdamDunmoreCollege <151739494+AdamDunmoreCollege@users.noreply.github.com>
2024-05-17 10:47:12 +0100

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,15 @@
 export default function Home(){
     return `
     <section class="home_section home_hdr">
-        <img id="home_header_img" src="/src/assets/img/home_1.jpg"/>
+        <img id="home_header_img" src="./assets/img/home_1.jpg"/>
         <p id="home_header_text">Granton Gas Works</p>
     </section>
     <section class="home_section home_rtl">
-        <img class="home_img" src="/src/assets/img/home_2.png"/>
+        <img class="home_img" src="./assets/img/home_2.png"/>
         <p class="home_text">Welcome to the Granton Gas Holder, a remarkable landmark nestled in the heart of Edinburgh's vibrant Granton area. This iconic structure stands as a testament to the city's rich industrial heritage and innovative spirit. Originally built to store gas for the burgeoning city, the Granton Gas Holder has been thoughtfully preserved, offering a unique glimpse into Edinburgh's past. As you explore this fascinating site, you'll discover a blend of historical significance and modern charm, making it a must-visit destination for history enthusiasts and curious visitors alike. Enjoy your visit and immerse yourself in the captivating story of Granton's industrial legacy!</p>
     </section>
     <section class="home_section home_ltr">
-        <img class="home_img" src="/src/assets/img/home_3.jpg"/>
+        <img class="home_img" src="./assets/img/home_3.jpg"/>
         <p class="home_text">Welcome to the Granton Gas Holder, a remarkable landmark nestled in the heart of Edinburgh's vibrant Granton area. This iconic structure stands as a testament to the city's rich industrial heritage and innovative spirit. Originally built to store gas for the burgeoning city, the Granton Gas Holder has been thoughtfully preserved, offering a unique glimpse into Edinburgh's past. As you explore this fascinating site, you'll discover a blend of historical significance and modern charm, making it a must-visit destination for history enthusiasts and curious visitors alike. Enjoy your visit and immerse yourself in the captivating story of Granton's industrial legacy!</p>
     </section>
     `;
