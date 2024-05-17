7c2112f48e022262fe9a52490cb078dad66d675c
AdamDunmoreCollege <151739494+AdamDunmoreCollege@users.noreply.github.com>
2024-05-17 10:47:37 +0100

    Update history.js

--- src/pages/history.js
+++ src/pages/history.js
@@ -2,12 +2,12 @@ export default function History(){
     return `
        <div id="history_wrapper">     
             <section class="history_section">
-                <img class="history_img" src="/src/assets/img/history_1.jpg"/>
+                <img class="history_img" src="./assets/img/history_1.jpg"/>
                 <p class="history_text">The Granton Gas Holder, an iconic feature of Edinburgh's skyline, dates back to the late 19th century when it was constructed to meet the city's growing demand for gas. Originally part of a larger gasworks complex, it played a crucial role in storing and regulating the gas supply for the city’s homes and businesses. The structure, with its intricate ironwork and imposing stature, exemplifies the industrial ingenuity of its era. Despite the decline of gasworks in the mid-20th century, the Granton Gas Holder has been preserved as a historical landmark, symbolizing the city’s rich industrial past and its evolution through the ages. Today, it stands not only as a piece of functional history but also as a striking example of Victorian engineering and architectural design.</p>
             </section>
 
             <section class="history_section">
-                <img class="history_img" src="/src/assets/img/history_2.jpg"/>
+                <img class="history_img" src="./assets/img/history_2.jpg"/>
                 <p class="history_text">In recent years, the Granton Gas Holder has undergone significant preservation efforts to maintain its structural integrity and historical significance. This has included careful restoration to prevent deterioration and ensure the safety of visitors. The Gas Holder is now a focal point for community events and cultural activities, bridging the gap between Edinburgh's industrial heritage and its vibrant contemporary culture. Its surrounding area is being revitalized with new developments that respect the historical context while introducing modern amenities. As a result, the Granton Gas Holder continues to inspire and attract visitors, serving as a poignant reminder of the city’s industrious past and its ongoing transformation.</p>
             </section>
         </div>
